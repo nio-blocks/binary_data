8ad4cb41446f315b2ae3683ec2fdbb1432b935b6
Tom Young <tyoung@n.io>
2018-04-06 14:31:55 -0600

    Initial commit

---
+++ pack_bytes_block.py
@@ -0,0 +1,12 @@
+from nio import Block
+from nio.properties import VersionProperty
+
+
+class PackBytes(Block):
+
+    version = VersionProperty('0.1.0')
+
+    def process_signals(self, signals):
+        for signal in signals:
+            pass
+        self.notify_signals(signals)
--- tests/test_pack_bytes_block.py
+++ tests/test_pack_bytes_block.py
@@ -1,14 +1,14 @@
 from nio.block.terminals import DEFAULT_TERMINAL
 from nio.signal.base import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
-from ..example_block import Example
+from ..pack_bytes_block import PackBytes
 
 
-class TestExample(NIOBlockTestCase):
+class TestPackBytes(NIOBlockTestCase):
 
     def test_process_signals(self):
         """Signals pass through block unmodified."""
-        blk = Example()
+        blk = PackBytes()
         self.configure_block(blk, {})
         blk.start()
         blk.process_signals([Signal({"hello": "nio"})])
