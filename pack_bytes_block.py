680c130b73f40ee78899f306264c70406e5e0071
Tom Young <tyoung@n.io>
2018-05-22 17:23:34 -0600

    prototype

--- pack_bytes_block.py
+++ pack_bytes_block.py
@@ -1,18 +1,83 @@
-from struct import pack
+from enum import Enum
+from struct import pack, error
 from nio import Block, Signal
-from nio.properties import Property, StringProperty, VersionProperty
+from nio.properties import Property, BoolProperty, ListProperty, \
+                           SelectProperty, StringProperty, VersionProperty, \
+                           PropertyHolder
+
+
+class Length(Enum):
+    two = 2
+    four = 4
+    eight = 8
+
+class PythonType(Enum):
+    unsigned_integer = 'uint'
+    integer = 'int'
+    float = 'float'
+
+class Endian(Enum):
+    little = '<'
+    big = '>'
+
+class NewAttributes(PropertyHolder):
+    format = SelectProperty(PythonType,
+                            title='Format',
+                            default=PythonType.integer)
+    endian = SelectProperty(Endian, title='Endian', default=Endian.big)
+    key = StringProperty(title='New Attribute Key', default='{{ $key }}')
+    value = Property(title='Value to Pack', default='{{ $value }}')
+    length = SelectProperty(Length,
+                            title='Packed Length (Bytes)',
+                            default=Length.four)
 
 
 class PackBytes(Block):
 
-    format = StringProperty(title='Format Character', default='')
-    order = StringProperty(title='Byte Order Character', default='')
-    data = Property(title='Data to Pack')
+    new_attributes = ListProperty(NewAttributes,
+                                  title='New Signal Attributes',
+                                  default=[{'format': 'integer',
+                                             'endian': 'big',
+                                             'key': '{{ $key }}',
+                                             'value': '{{ $value }}',
+                                             'length': 'four'}])
     version = VersionProperty('0.1.0')
 
     def process_signals(self, signals):
         outgoing_signals = []
         for signal in signals:
-            bytes = pack(self.format(signal), self.data(signal))
-            outgoing_signals.append(Signal({'data': bytes}))
+            new_signal_dict = {}
+            for attr in self.new_attributes():
+                _value = attr.value(signal)
+                _type = attr.format(signal).value
+                _endian = attr.endian(signal).value
+                _length = attr.length(signal).value
+                fmt_char = None
+                if _type in ['int', 'uint']:
+                    if _length == 2:
+                        fmt_char = 'h'
+                    elif _length == 4:
+                        fmt_char = 'i'
+                    elif _length == 8:
+                        fmt_char = 'q'
+                    if _type == 'uint':
+                        fmt_char = fmt_char.upper()
+                else:
+                    if _length == 2:
+                        fmt_char = 'e'  # added in python 3.6
+                    if _length == 4:
+                        fmt_char = 'f'
+                    elif _length == 8:
+                        fmt_char = 'd'
+                fmt = _endian + fmt_char
+                try:
+                    value = pack(fmt, _value)
+                except error as e:
+                    if e.args[-1] == 'bad char in struct format':
+                        self.logger.error('Python >= 3.6 is required to '
+                                          'pack a float into 2 bytes')
+                    raise e
+                new_signal_dict[attr.key(signal)] = value
+            if new_signal_dict:
+                outgoing_signals.append(Signal(new_signal_dict))
         self.notify_signals(outgoing_signals)
--- tests/test_pack_bytes_block.py
+++ tests/test_pack_bytes_block.py
@@ -1,3 +1,5 @@
+from struct import error
+from unittest.mock import MagicMock, patch
 from nio.block.terminals import DEFAULT_TERMINAL
 from nio.signal.base import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
@@ -9,11 +11,75 @@ class TestPackBytes(NIOBlockTestCase):
     def test_process_signals(self):
         """Pack incoming values"""
         blk = PackBytes()
-        self.configure_block(blk, {'format': '{{ $type }}',
-                                   'order': '{{ $endian }}',
-                                   'data': '{{ $value }}'})
+        self.configure_block(blk, {})
         blk.start()
-        blk.process_signals([Signal({'type': 'i', 'endian': '', 'value': 42})])
+        blk.process_signals([Signal({'key': 'foo', 'value': 42})])
         blk.stop()
         self.assert_num_signals_notified(1)
-        self.assert_last_signal_notified(Signal({'data': b'\x2A\x00\x00\x00'}))
+        self.assert_last_signal_notified(Signal({'foo': b'\x00\x00\x00\x2A'}))
+
+    def test_multiple_values(self):
+        """Unpack two integers from one byte array"""
+        blk = PackBytes()
+        self.configure_block(
+            blk,
+            {'new_attributes': [{'key': 'one', 'value': '{{ $value_1 }}'},
+                                {'key': 'two', 'value': '{{ $value_2 }}'}]})
+        blk.start()
+        blk.process_signals([Signal({'value_1': 42, 'value_2': -42})])
+        blk.stop()
+        self.assert_num_signals_notified(1)
+        self.assert_last_signal_notified(Signal({'one': b'\x00\x00\x00*',
+                                                 'two': b'\xff\xff\xff\xd6'}))
+
+    def test_dynamic_data_types(self):
+        """Unpack incoming bytes according to signal evaluation"""
+        blk = PackBytes()
+        self.configure_block(
+            blk,
+            {'new_attributes': [{'format': '{{ $format }}',
+                                 'endian': '{{ $endian }}',
+                                 'length': '{{ $length }}',
+                                 'key': '{{ $key }}'}]})
+        blk.start()
+        signal_list = [Signal({'format': 'unsigned_integer',
+                               'endian': 'big',
+                               'length': 2,
+                               'key': 'uint',
+                               'value': 255}),
+                       Signal({'format': 'integer',
+                               'endian': 'little',
+                               'length': 4,
+                               'key': 'int',
+                               'value': -256}),
+                       Signal({'format': 'float',
+                               'endian': 'big',
+                               'length': 8,
+                               'key': 'float',
+                               'value': 0.0})]
+        blk.process_signals(signal_list)
+        blk.stop()
+        self.assert_last_signal_list_notified([
+            Signal({'uint': b'\x00\xFF'}),
+            Signal({'int': b'\x00\xFF\xFF\xFF'}),
+            Signal({'float': b'\x00' * 8})])
+
+    @patch(PackBytes.__module__ + '.pack')
+    def test_format_not_available(self, mock_unpack):
+        """Log a helpful error if the installed python version does not
+        support the format specified"""
+        mock_unpack.side_effect = error('bad char in struct format')
+        blk = PackBytes()
+        # py3.6+ is required to pack a float into 2 bytes, format char 'e'
+        self.configure_block(blk, {'new_attributes': [{'format': 'float',
+                                                       'key': 'foo',
+                                                       'length': 'two',
+                                                       'value': 42}]})
+        blk.logger = MagicMock()
+        blk.start()
+        with self.assertRaises(error):
+            blk.process_signals([Signal()])
+        blk.stop()
+        blk.logger.error.assert_called_once_with(
+            'Python >= 3.6 is required to pack a float into 2 bytes')
+        self.assert_num_signals_notified(0)
